93f72032ec4010a1cf88de039f3ea1ab0d4fee07
Maksim Egorov <m.egorov193@gmail.com>
2023-11-16 15:33:19 +0400

    upd TableResultFormComponent

--- src/test/java/ru/egorovma/pages/components/TableResultFormComponent.java
+++ src/test/java/ru/egorovma/pages/components/TableResultFormComponent.java
@@ -2,6 +2,7 @@ package ru.egorovma.pages.components;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -10,6 +11,6 @@ public class TableResultFormComponent {
     private final SelenideElement tableResponsiveInput = $(".table-responsive");
 
     public void checkResult(String key, String value) {
-        tableResponsiveInput.$(byText(key)).sibling(0).shouldHave(text(value));
+        tableResponsiveInput.should(appear).$(byText(key)).sibling(0).shouldHave(text(value));
     }
 }
