877b6b00a5a6f6329855121394a6f3ff706a2b5a
egorov-ma <66911064+egorov-ma@users.noreply.github.com>
2023-11-17 19:46:11 +0400

    Update CalendarComponent.java

--- src/test/java/ru/egorovma/pages/components/CalendarComponent.java
+++ src/test/java/ru/egorovma/pages/components/CalendarComponent.java
@@ -5,7 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
-    private final static SelenideElement yearInput = $(".react-datepicker__year-select"),
+    private final SelenideElement yearInput = $(".react-datepicker__year-select"),
             monthInput = $(".react-datepicker__month-select");
 
     public void setDate(String day, String month, String year) {
