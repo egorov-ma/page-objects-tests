2059031de7204934cd3f2687e928316bdecc599d
Maksim Egorov <m.egorov193@gmail.com>
2023-11-27 17:43:01 +0400

    upd CalendarComponent

--- src/test/java/ru/egorovma/pages/components/CalendarComponent.java
+++ src/test/java/ru/egorovma/pages/components/CalendarComponent.java
@@ -2,17 +2,17 @@ package ru.egorovma.pages.components;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
     private final SelenideElement chooseYear = $(".react-datepicker__year-select"),
-            chooseMonth = $(".react-datepicker__month-select"),
-            chooseDay = $(".react-datepicker__month:not(.react-datepicker__day--outside-month)");
+            chooseMonth = $(".react-datepicker__month-select");
 
     public void setDate(String day, String month, String year) {
         chooseYear.selectOption(year);
         chooseMonth.selectOption(month);
-        chooseDay.$(byText(day)).click();
+        $(".react-datepicker__month").$$(".react-datepicker__day:not(.react-datepicker__day--outside-month)")
+                .findBy(text(day)).click();
     }
 }
